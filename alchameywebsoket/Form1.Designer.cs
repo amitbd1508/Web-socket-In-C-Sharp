517d1d75473330495641baca9c586e394997f6da
Amit Ghosh <amitbd1508@gmail.com>
2017-09-08 22:20:53 +0600

    inital commit

---
+++ alchameywebsoket/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace AlchameyWebsoket
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TotalPice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(703, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(365, 313);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.TotalPice});
+            this.dataGridView1.Location = new System.Drawing.Point(3, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dataGridView1.Size = new System.Drawing.Size(668, 342);
+            this.dataGridView1.StandardTab = true;
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Column1
+            // 
+            this.Column1.FillWeight = 37.45318F;
+            this.Column1.HeaderText = "Table";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.DividerWidth = 1;
+            this.Column2.FillWeight = 162.5468F;
+            this.Column2.HeaderText = "Orders";
+            this.Column2.MinimumWidth = 10;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // TotalPice
+            // 
+            this.TotalPice.HeaderText = "Total Price";
+            this.TotalPice.Name = "TotalPice";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 366);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TotalPice;
+    }
+}
+
---
+++ alchameywebsoket/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Alchemy;
+using Alchemy.Classes;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+
+namespace AlchameyWebsoket
+{
+    public partial class Form1 : Form
+    {
+        int rowindex = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            startServer();
+        }
+
+        void startServer()
+        {
+            var aServer = new WebSocketServer(8182, IPAddress.Any)
+            {
+
+                OnReceive = OnReceive,
+                OnSend = OnSend,
+                OnConnect = OnConnect,
+                OnConnected = OnConnected,
+                OnDisconnect = OnDisconnect,
+                TimeOut = new TimeSpan(0, 5, 0)
+            };
+            richTextBox1.AppendText("Initializing......\n");
+            aServer.Start();
+            richTextBox1.AppendText("Start at " + aServer.ListenAddress+":8182" + "\n");
+        }
+        
+
+        private void OnDisconnect(UserContext context)
+        {
+            
+            Invoke(new Action(() => richTextBox1.AppendText(context.ClientAddress+"Disconnected............." + "\n")));
+            
+        }
+
+        private void OnConnect(UserContext context)
+        {
+
+            Invoke(new Action(() => richTextBox1.AppendText(context.ClientAddress + "OnConnect............." + "\n")));
+        }
+
+        private void OnSend(UserContext context)
+        {
+           
+            Invoke(new Action(() => richTextBox1.AppendText("OnSend..............\n")));
+        }
+
+        private void OnReceive(UserContext context)
+        {
+            Invoke(new Action(() => richTextBox1.AppendText(context.DataFrame+"\n" )));
+            ////context.Send("hi");
+            //JArray arry = JArray.Parse(context.DataFrame.ToString());
+            
+            ////dataGridView1.Rows[rowindex].Cells[1].Value = s;
+            //DataGridViewRow row = (DataGridViewRow)dataGridView1.Rows[0].Clone();
+            //row.Cells[0].Value = "Table 1";
+            //dataGridView1.Columns[1].DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            //string s="";
+            //for (int i = 0; i < arry.Count-1;i++ )
+            //    s =s+ arry[i]+Environment.NewLine;
+            //row.Cells[1].Value = s;
+            //row.Cells[2].Value = arry[arry.Count - 1];
+            //Invoke(new Action(() => dataGridView1.Rows.Add(row)));
+            
+
+        }
+        private void OnConnected(UserContext context)
+        {
+            
+            Invoke(new Action(() => richTextBox1.AppendText("Client Connection From : " + context.ClientAddress.ToString())));
+        }
+    }
+}
